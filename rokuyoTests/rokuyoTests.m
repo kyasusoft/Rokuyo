7ae13a76d818e52a8f3daba70870aad814874d27
tototti <tototti@gmail.com>
2016-12-06 19:11:33 +0900

    テストコードを書き換え ・1ヶ月分を全てチェック ・通常の月(2016/12)を追加 ・範囲外のテスト

--- rokuyoTests/rokuyoTests.m
+++ rokuyoTests/rokuyoTests.m
@@ -25,31 +25,48 @@
     [super tearDown];
 }
 
+/// 2000_07 = 月に二回旧暦が変わる
 - (void)test2000_07 {
 
+    NSArray* rokuyoArray = @[@"仏滅",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"先勝"];
+    
     KYRokuyo* rokuyo = [[KYRokuyo alloc] init];
-    // 2000-7-2に 旧暦の月が替わる
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2000 month:7 day:1], @"仏滅");
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2000 month:7 day:2], @"赤口");
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2000 month:7 day:3], @"先勝");
-
-    // 2000-7-31に 旧暦の月が替わる
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2000 month:7 day:30], @"仏滅");
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2000 month:7 day:31], @"先勝");
+    for (int i = 0; i < 31; i++) {
+        XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2000 month:7 day:i+1], rokuyoArray[i]);
+    }
 }
 
+/// 2005_12 月に二回旧暦が変わる
 - (void)test2005_12 {
-    KYRokuyo* rokuyo = [[KYRokuyo alloc] init];
+    NSArray* rokuyoArray = @[@"先負",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"赤口"];
 
-    // 2005-12-2に 旧暦の月が替わる
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2005 month:12 day:1], @"先負");
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2005 month:12 day:2], @"大安");
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2005 month:12 day:3], @"赤口");
+    KYRokuyo* rokuyo = [[KYRokuyo alloc] init];
+    for (int i = 0; i < 31; i++) {
+        XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2005 month:12 day:i+1], rokuyoArray[i]);
+    }
+}
+
+/// 2016_12 ノーマルな月のテスト
+- (void)test2016_12 {
+    NSArray* rokuyoArray = @[@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"大安",@"赤口",@"先勝",@"友引",@"先負",@"仏滅",@"赤口",@"先勝",@"友引"];
     
-    // 2000-12-31に 旧暦の月が替わる
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2005 month:12 day:30], @"先負");
-    XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2005 month:12 day:31], @"赤口");
-    
+    KYRokuyo* rokuyo = [[KYRokuyo alloc] init];
+    for (int i = 0; i < 31; i++) {
+        XCTAssertEqualObjects([rokuyo sinrekiToRokuyoWithYear:2016 month:12 day:i+1], rokuyoArray[i]);
+    }
+}
+
+/// 範囲外のテスト(下限)
+- (void)test1999_12 {
+    KYRokuyo* rokuyo = [[KYRokuyo alloc] init];
+    XCTAssertNil([rokuyo sinrekiToRokuyoWithYear:1999 month:12 day:31]);
+}
+
+
+/// 範囲外のテスト
+- (void)test2033_01 {
+    KYRokuyo* rokuyo = [[KYRokuyo alloc] init];
+    XCTAssertNil([rokuyo sinrekiToRokuyoWithYear:2033 month:1 day:1]);
 }
 
 @end
